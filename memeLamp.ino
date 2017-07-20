1a10fd126f7ab2257b2e7ab908ac45183bfcd296
Ben <ben.winding@gmail.com>
2017-07-20 22:55:37 +0930

    v1.5

--- memeLamp.ino
+++ memeLamp.ino
@@ -1,11 +1,10 @@
+#include <ArduinoJson.h>
+#include "WiFiEsp.h"
 #include <StandardCplusplus.h>
 #include <serstream>
 #include <string>
 #include <vector>
 #include <iterator>
-#include <ArduinoJson.h>
-
-#include "WiFiEsp.h"
 
 // Emulate Serial1 on pins 7/6 if not present
 #ifndef HAVE_HWSERIAL1
@@ -15,7 +14,6 @@ SoftwareSerial Serial1(6, 7); // RX, TX
 
 const char* ssid = "The LANnisters 2.4";
 const char* pwd= "huskyvalley292";
-
 //char ssid[] = "Ben's iPhone";
 //char pwd[] = "reallylongpassword";
 
@@ -61,36 +59,31 @@ void ReadClient(WiFiEspClient client)
 {
   long _startMillis = millis();
   while (!client.available() and (millis()-_startMillis < 2000))
-  {
-  }
+  {}
 
+  String readBuffer;
+  
   char c;
-  bool beginPrint = false;
-  int byteCount = 300;
-  std::vector<int> last = {1,2,3,4,5};
-  while((c = client.read()) > 0 and (byteCount > 0)){
-    if (c == '{' )
-      beginPrint = true;      
-    if(beginPrint)
-      Serial.print(c);
-    else{
-      if(last.size() > 30) {
-        last.erase(last.begin());        
-      } 
-      last.push_back((int) c);        
+  std::vector<int> last = {0,0,0,0};
+  while((c = client.read()) > 0 ){
+    last.erase(last.begin());        
+    last.push_back(c);
+    
+    if (last[0] == 13 && last[1] == 10 && last[2] == 13 && last[3] == 10) {
+      // [13][10][13][10] <- end of header characters
+      Serial.println(c);
+      readBuffer = client.readString();
+      break;
     }
-    byteCount--;
   }
 
+  Serial.println(readBuffer);
   Serial.println();
-  Serial.print("Vector is size: "); Serial.println((int) last.size());
-  Serial.println("Last characters printed: ");
-  for (int i=0; i< last.size(); i++)
-  {
-    Serial.print("[");
-    Serial.print((int) last.at(i));
-    Serial.print("]");
-  }
-  Serial.println();
+
+  StaticJsonBuffer<500> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(readBuffer);
+  const char* dank = root["dank"];
+  Serial.print("Dank is:");   
+  Serial.println(dank);
 }
 
