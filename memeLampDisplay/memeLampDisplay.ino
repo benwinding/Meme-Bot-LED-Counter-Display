f4175847c2ef0546ce059898e5c5d1a2dd6ed0f0
Ben <ben.winding@gmail.com>
2017-07-20 23:03:31 +0930

    Version 5.2!

--- memeLampDisplay/memeLampDisplay.ino
+++ memeLampDisplay/memeLampDisplay.ino
@@ -12,18 +12,17 @@
 
 Display disp;
 
-
 void setup()
 {
   disp.Init();
-  disp.LoadMsg(1, "$$$  Meme Bot  $$$");
-  disp.LoadMsg(2, "+++ LIVE COUNT +++");
-  disp.LoadMsg(3, "==================");
-  disp.LoadMsg(4, "    Version 5.1  ");
+  disp.LoadMsg(1, F("$$$  Meme Bot  $$$"));
+  disp.LoadMsg(2, F("+++ LIVE COUNT +++"));
+  disp.LoadMsg(3, F("=================="));
+  disp.LoadMsg(4, F("    Version 5.1   "));
   Serial.begin(115200);
-  // while(!Serial){}
+  // while(!Serial){} // Required for the Leonardo
 
-  //ProcessJson("{\"dank\":\"420\"}");
+  // ProcessJson("{\"dank\":\"420\"}");
 
   Wire.begin(8);
   Wire.onReceive(ReceiveWire); // register event
@@ -56,15 +55,14 @@ void ReceiveWire() {
   }
 }
 
-const char* rowLookup = "{\"meme\":\"1\",\"dank\":\"2\",\"hot\":\"3\",\"random\":\"4\",\"xxx\":\"3\",\"welcome\":\"4\",\"find\":\"4\",\"help\":\"4\",\"how\":\"4\",\"why\":\"4\",\"share\":\"4\"}";
-
 void ProcessJson(String jsonString) {
-  StaticJsonBuffer<200> jsonMainBuffer;
-  JsonObject& rootLookup = jsonMainBuffer.parseObject(rowLookup);
-
+  Serial.print(F("Recieved: "));
   Serial.println(jsonString);
+
   StaticJsonBuffer<200> jsonBuffer1;
   JsonObject& rootR = jsonBuffer1.parseObject(jsonString);
+  StaticJsonBuffer<200> jsonMainBuffer;
+  JsonObject& rootLookup = jsonMainBuffer.parseObject(F("{\"meme\":\"1\",\"dank\":\"2\",\"hot\":\"3\",\"random\":\"4\",\"xxx\":\"3\",\"welcome\":\"4\",\"find\":\"4\",\"help\":\"4\",\"how\":\"4\",\"why\":\"4\",\"share\":\"4\"}"));
 
   for(JsonObject::iterator it=rootR.begin(); it!=rootR.end(); ++it) 
   {
@@ -83,6 +81,6 @@ void CheckInput() {
     return;
   char c = Serial.read();
   String letter = String(c);
-  disp.LoadMsg(4,letter);
+  disp.LoadMsg(1,letter);
 }
 
